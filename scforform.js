38d60b59bad1e013d6b7f4cc79e868e99858d443
Amin Naji <aminnaji2018@gmail.com>
2021-05-30 19:58:28 +0430

    myfirstwebsite

---
+++ scforform.js
@@ -0,0 +1,39 @@
+function sum()
+{
+    var str1;
+    var str2;
+    var str3;
+    var str4;
+    var str5;
+    var str6;
+    var str7;
+    var str8;
+    var str9;
+    var str10;
+    var str11;
+    var str12;
+    var str13;
+    var str14;
+    var str15;
+    var str16;
+
+    str1=document.getElementById("fname").nodeValue;
+    str2=document.getElementById("lname").nodeValue;
+    str3=document.getElementById("emailadd").nodeValue;
+    str4=document.getElementById("pwd").nodeValue;
+    str5=document.getElementById("male").nodeValue;
+    str6=document.getElementById("female").nodeValue;
+    str7=document.getElementById("other").nodeValue;
+    str8=document.getElementById("instrument1").nodeValue;
+    str9=document.getElementById("instrument2").nodeValue;
+    str10=document.getElementById("instrument3").nodeValue;
+    str11=document.getElementById("instrument4").nodeValue;
+    str12=document.getElementById("instrument5").nodeValue;
+    str13=document.getElementById("instrument6").nodeValue;
+    str14=document.getElementById("duration").nodeValue;
+    str15=document.getElementById("agree").nodeValue;
+    str16=document.getElementById("disagree").nodeValue;
+    
+}
+
+alert(str1+"..."+str2+"..."+str3+"..."+str4+"..."+str5+"..."+str6+"..."+str7+"..."+str8+"..."+str9+"..."+str10+"..."+str11+"..."+str12+"..."+str13+"..."+str14+"..."+str15+"..."+str16);
